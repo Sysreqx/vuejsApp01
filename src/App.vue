e8acbe4e1d87e08177f67c375a2cd1948b12125b
Kaisar Yerdenbekov <sysreqx@gmail.com>
2024-01-16 10:50:53 +0600

    feature (all): "@/" = alias referring to the "src" folder

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <script>
+  import PostForm from "@/components/PostForm.vue";
+  import PostList from "./components/PostList.vue";
+
   export default {
     data() {
       return {
@@ -13,7 +16,15 @@
     },
     methods: {
       createPost() {
+        const newPost = {
+          id: Date.now(),
+          title: this.title,
+          body: this.body,
+        }
 
+        this.posts.push(newPost);
+        this.title = "";
+        this.body = "";
       },
 
       // inputTitle(event) {
@@ -25,17 +36,9 @@
 
 <template>
   <div class="app">
-    <form action="">
-      <h4>Создание поста</h4>
-      <input v-bind:value="title" @input="title = $event.target.value" type="text" class="input" placeholder="Название">
-      <input v-bind:value="body" @input="body = $event.target.value" type="text" class="input" placeholder="Описание">
-      <button class="btn" @click="createPost">Создать</button>
-    </form>
 
-    <div class="post" v-for="post in posts">
-        <div><strong>Название:</strong> {{ post.title }}</div>
-        <div><strong>Описание:</strong> {{ post.body }}</div>
-    </div>
+
+
   </div>
 </template>
 
@@ -48,32 +51,5 @@
 
   .app {
     padding: 20px;
-  }
-
-  .post {
-    margin-top: 15px;
-    padding: 15px;
-    border: 2px solid teal;
-  }
-
-  form {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .input {
-    width: 100%;
-    border: 1px solid teal;
-    padding: 10px 15px;
-    margin-top: 15px;
-  }
-
-  .btn {
-    margin-top: 15px;
-    align-self: flex-end;
-    padding: 10px 15px;
-    background: none;
-    color: teal;
-    border: 1px solid teal;
   }
 </style>
