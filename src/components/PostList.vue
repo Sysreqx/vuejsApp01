3c90374b2bed08877acc87f2f686ccc03709b329
Kaisar Yerdenbekov <sysreqx@gmail.com>
2024-01-16 10:51:43 +0600

    feature (all): Decomposition. Create reusable components

---
+++ src/components/PostList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="post" v-for="post in posts">
+    <div><strong>Название:</strong> {{ post.title }}</div>
+    <div><strong>Описание:</strong> {{ post.body }}</div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+  .post {
+    margin-top: 15px;
+    padding: 15px;
+    border: 2px solid teal;
+  }
+</style>
